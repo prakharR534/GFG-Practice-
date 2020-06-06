d10b6c2ac3fab977dbd7bfd549f28d0be47b3720
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-06-06 11:54:14 +0530

    Create Split the input - Python.py

---
+++ Split the input - Python.py
@@ -0,0 +1,25 @@
+#User function Template for python3
+
+def inPutS():
+    a=input() ## input in a single line()
+    s,i,f = a.split(" ")## split the a into three parts: string, integer, and f. 
+    print(s+" "+str(int(i)+float(f))) ##type cast i to int, f to float. Add i with f. Typecast the result to string
+
+
+
+#{ 
+#  Driver Code Starts
+
+
+
+def main():
+    testcases=int(input()) #testcases
+    while(testcases>0):
+        inPutS() #the function that gets things done
+        testcases-=1
+        
+
+
+if __name__=='__main__':
+    main()
+# } Driver Code Ends
