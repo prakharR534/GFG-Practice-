98fa43873ba8ff95a2873cd17e75e1b5990d5a28
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-06-13 21:46:54 +0530

    Create Reverse a string.cpp

---
+++ Reverse a string.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+ {
+	int i,t,j,k;
+char s[1000];
+cin>>t;
+
+for(i=0;i<t;i++) {
+    cin>>s;
+    int l = strlen(s);
+    for(j=l-1;j>=0;j--)
+{
+//printf("%c",s[j]);
+cout<<s[j];
+}
+cout<<endl;
+    
+    
+}
+	//code
+	return 0;
+}
+
+
+
