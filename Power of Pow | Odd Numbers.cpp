001727f2f211d13e43bfdb21e3b4214eaa0cf027
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-06-15 00:03:51 +0530

    Create Power of Pow | Odd Numbers.cpp

---
+++ Power of Pow | Odd Numbers.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+ {
+		int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    long long int sum=0;
+	    
+	    for(int i=0;i<n;i++ ){
+	        sum += pow(2*i+1,2);
+	        
+	    }
+	    
+	    
+	    cout<<sum<<endl;
+	}
+	return 0;
+}
