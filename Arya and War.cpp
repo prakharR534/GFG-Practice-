f7c3fbc30b1e7c25c0843712fa366a6d4abc072e
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-06-16 12:58:08 +0530

    Create Arya and War.cpp

---
+++ Arya and War.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+ {
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    int n,count=0;
+	    cin>>n;
+	    
+	    while(n){
+	        if(n & 1 ==1){
+	            count++;
+	        }
+	        n = n>>1;
+	    }
+	    cout<<count<<endl;
+	}
+	//code
+	return 0;
+}
