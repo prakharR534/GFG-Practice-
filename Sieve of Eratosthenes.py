af85e48dcee940bb9afb6207078576b176825ad3
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-06-24 21:40:37 +0530

    Create Sieve of Eratosthenes.py

---
+++ Sieve of Eratosthenes.py
@@ -0,0 +1,27 @@
+#code
+
+
+t = int(input())
+while(t):
+    n = int(input())
+    
+    
+    prime = [True for i in range(n+1)]
+    p = 2
+    while (p*p <= n):
+        
+        if(prime[p] == True):
+            
+            for i in range(p * p , n+1 , p):
+                prime[i] = False
+                
+        p += 1
+        
+        
+    for p in range(2,n+1):
+        if prime[p]:
+            print (p,end=" ") 
+            
+    print("")        
+            
+    t = t-1        
