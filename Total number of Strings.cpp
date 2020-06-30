9cbdd1a2a63ac3db9e9850c326477528d5434bbd
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-07-01 01:06:42 +0530

    Create Total number of Strings.cpp

---
+++ Total number of Strings.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+ {
+	int t;
+	cin>>t;
+	while(t--){
+	int j,k,m,n;
+	cin>>n;
+
+j = n*n*n;
+k = 3*n + 2;
+m = (j+k)/2;
+
+cout<<m<<endl; 
+	
+	}
+	//code
+	return 0;
+}
