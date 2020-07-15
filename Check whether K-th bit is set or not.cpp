a74504b202c75ff9034b7d310cb1b829acde754e
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-07-15 21:15:44 +0530

    Create Check whether K-th bit is set or not.cpp

---
+++ Check whether K-th bit is set or not.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+ {
+	int t;
+	cin>>t;
+	while(t--){
+	int n,k;
+	cin>>n>>k;
+	
+	n = n>>k;
+	
+	if(n%2 == 1){
+	    cout<<"Yes\n";
+	}
+	else cout<<"No\n";
+	
+	
+	
+	}
+	//code
+	return 0;
+}
