902fde82a36d96d32bec3f3d518a452047e82b31
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-07-19 21:16:33 +0530

    Create Set kth bit.cpp

---
+++ Set kth bit.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+ {
+	int t;
+	cin>>t;
+	while(t--){
+	int n,k,p;
+	cin>>n>>k;
+	
+	p = pow(2,k);
+	
+	n = n|p;
+	
+	cout<<n<<endl;
+	
+	
+	}
+	//code
+	return 0;
+}
