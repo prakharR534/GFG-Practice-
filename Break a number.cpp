9653a9ad1cfbd67218d5193510d9bdeff7861388
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-07-20 22:31:12 +0530

    Create Break a number.cpp

---
+++ Break a number.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+ {
+	int t;
+	cin>>t;
+	while(t--){
+	
+	
+	 int c,n;
+	    cin>>n;
+	    
+	    c=n*(n+1)/2+(n+1);
+	    cout<<c<<endl;
+	    
+	}
+	//code
+	return 0;
+}
