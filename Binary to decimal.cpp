35876e80278217bdfa145b3653d016646624bd75
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-07-25 20:51:27 +0530

    Create Binary to decimal.cpp

---
+++ Binary to decimal.cpp
@@ -0,0 +1,13 @@
+#include <math.h>
+#include <stdio.h>
+int toDecimal(int n)
+{
+  int dec = 0, i = 0, rem;
+    while (n != 0) {
+        rem = n % 10;
+        n /= 10;
+        dec += rem * pow(2, i);
+        ++i;
+    }
+    return dec;
+}
