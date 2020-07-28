5a1bf3cc8038a62f9c71f6b1a6a58e72b008b646
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-07-28 21:32:59 +0530

    Create Get minimum element from stack.cpp

---
+++ Get minimum element from stack.cpp
@@ -0,0 +1,115 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+class _stack{
+stack<int> s;
+int minEle;
+public :
+    int getMin();
+    int pop();
+    void push(int);
+};
+
+
+int main()
+{
+   int t;
+   cin>>t;
+   while(t--)
+   {
+       int q;
+       cin>>q;
+        _stack *a = new _stack();
+       while(q--){
+
+       int qt;
+       cin>>qt;
+
+       if(qt==1)
+       {
+           //push
+           int att;
+           cin>>att;
+           a->push(att);
+       }
+       else if(qt==2)
+       {
+           //pop
+           cout<<a->pop()<<" ";
+       }
+       else if(qt==3)
+       {
+           //getMin
+           cout<<a->getMin()<<" ";
+       }
+       }
+       cout<<endl;
+   }
+
+}
+
+
+// } Driver Code Ends
+
+
+/*
+The structure of the class is as follows
+class _stack{
+stack<int> s;
+int minEle;
+public :
+    int getMin();
+    int pop();
+    void push(int);
+};
+*/
+
+/*returns min element from stack*/
+int _stack :: getMin()
+{
+   //Your code here
+   if(s.empty()) return -1;
+   
+   return minEle;
+}
+
+/*returns poped element from stack*/
+int _stack ::pop()
+{
+   //Your code here
+   if(s.empty())return -1;
+   
+   if(s.top() >=0){
+       int temp = s.top();
+       s.pop();
+       return temp;
+       
+   }
+   else{
+       int temp = minEle;
+       minEle -= s.top();
+       s.pop();
+       return temp;
+   }
+   
+}
+
+/*push element x into the stack*/
+void _stack::push(int x)
+{
+   //Your code here
+   if(s.empty()){
+       s.push(x);
+       minEle = x;
+       return;
+   }
+   if(x>=minEle){
+       s.push(x);
+   }
+       else{
+           int temp = x - minEle;
+           s.push(temp);
+           minEle = x;
+       }
+   
+}
