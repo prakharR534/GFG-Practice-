c40ec26386e50a6ff42a7d52dbc6b811e8476cd5
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-07-31 13:58:44 +0530

    Create Triangular Number .cpp

---
+++ Triangular Number .cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+ {
+	int t;
+	cin>>t;
+	while(t--){
+	
+	int n;
+	cin>>n;
+	int x = sqrt(2*n);
+	if(x*(x+1)  == 2 * n)
+	cout<<"1\n";
+	else
+	cout<<"0\n";
+	
+	
+	}
+	//code
+	return 0;
+}
