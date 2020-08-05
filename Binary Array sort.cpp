2e120c7697289a1ad764ce59bbe0cfb84eee2678
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-08-05 14:31:14 +0530

    Create Binary Array sort.cpp

---
+++ Binary Array sort.cpp
@@ -0,0 +1,56 @@
+// { Driver Code Starts
+// A Sample C++ program for beginners with Competitive Programming
+#include <iostream>
+using namespace std;
+
+void binSort(int A[], int N);
+
+
+int main() {
+	int T;
+	cin>>T;
+	// Input the number of testcases
+	while(T--)
+	{
+	    int N;
+	    cin>>N; //Input size of array N
+	    int A[N]; 
+	    
+	    for(int i = 0; i  < N; i++)
+	      cin>>A[i];
+	      
+	    binSort(A,N);
+	    
+	    for(int x:A)
+	    cout<<x<<" ";
+	      
+	    cout<<endl;
+	}
+	return 0;
+}
+
+
+
+// } Driver Code Ends
+
+
+// Function to sort the array A[]
+// A[]: input array
+// N: input array
+void binSort(int A[], int N)
+{
+   int l=0,r=N-1;
+   while(l<r){
+       if(A[l] == 1){
+           swap(A[l],A[r]);
+           r--;
+       }
+       else
+       l++;
+   }
+   
+   
+   /**************
+    * No need to print the array
+    * ************/
+}
