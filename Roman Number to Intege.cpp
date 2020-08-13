05394fc65ea0a24be3b7dc460afedcb8131be3ba
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-08-13 22:34:00 +0530

    Create Roman Number to Intege.cpp

---
+++ Roman Number to Intege.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+ {
+	int t;
+	cin>>t;
+	while(t--){
+	    string s;
+	    cin>>s;
+	    
+	    unordered_map<char,int> m;
+	    m['I']=1;
+	    m['V']=5;
+	    m['X']=10;
+	    m['L']=50;
+	    m['C']=100;
+	    m['D']=500;
+	    m['M']=1000;
+	    
+	    int ans=0;
+	    
+	    for(int i=0;i<s.length();i++){
+	        if(m[s[i]] < m[s[i+1]] && i<s.length()-1){
+	            ans -=m[s[i]];
+	        }
+	        else{
+	            ans +=m[s[i]];
+	        }
+	    }
+	    
+	
+	 cout << ans << "\n";
+	
+	}
+	//code
+	return 0;
+}
+
