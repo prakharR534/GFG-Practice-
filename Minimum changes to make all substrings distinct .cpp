55aa26da3f0466a591687d012786be4888dfa8c2
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-09-25 23:44:56 +0530

    Create Minimum changes to make all substrings distinct .cpp

---
+++ Minimum changes to make all substrings distinct .cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+ {
+	int t;
+	cin>>t;
+	while(t--){
+	int n,k;
+	int A[n];
+	for(int i=0;i<n;i++)
+	cin>>A[i];
+	
+	int start=0;
+	int last=n-1;
+	while(start<=last){
+	    int mid = (start+last)/2;
+	    
+	    
+	    
+	}
+	
+	
+	}
+	//code
+	return 0;
+}
