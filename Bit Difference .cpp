3faff445f0d8c4890a43e22683f8738bf8fd8a4b
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-10-13 22:14:58 +0530

    Create Bit Difference .cpp

---
+++ Bit Difference .cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+ {
+	int t;
+	cin>>t;
+	while(t--){
+	int a,b,c=0;
+	cin>>a>>b;
+	int n=a^b;
+	while(n){
+	    if(n%2)
+	    c++;
+	    n/=2;
+	}
+	
+	cout<<c<<endl;
+	
+	
+	}
+	//code
+	return 0;
+}
+ 
