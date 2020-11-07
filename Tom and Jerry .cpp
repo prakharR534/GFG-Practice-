e0447f6020bd3f54ef0612a7c33028e542d825de
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-11-07 23:46:57 +0530

    Create Tom and Jerry .cpp

---
+++ Tom and Jerry .cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+ {
+	int t;
+	cin>>t;
+	while(t--){
+	int n;
+	cin>>n;
+	if(n%2==0)
+	cout<<"1\n";
+	else 
+	cout<<"0\n";
+	
+	
+	}
+	//code
+	return 0;
+}
